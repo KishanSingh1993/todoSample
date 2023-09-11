b5cdcf6d1f7e9506511e21a1ca29b886e84dea01
shivam.gupta9 <kishang.90@gmail.com>
2023-09-11 13:18:06 +0530

    initial

---
+++ app/src/main/java/in/ktechnos/todoassignment/MainActivity.kt
@@ -0,0 +1,123 @@
+package `in`.ktechnos.todoassignment
+
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import `in`.ktechnos.todoassignment.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(),TodoAdapter.TodoClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var database: TodoDatabase
+    lateinit var viewModel: TodoViewModel
+    lateinit var adapter: TodoAdapter
+    val REQUEST_CODE = 200
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        ).get(TodoViewModel::class.java)
+
+        viewModel.allTodo.observe(this) { list ->
+            list?.let {
+                adapter.updateList(list)
+            }
+        }
+
+        database = TodoDatabase.getDatabase(this)
+    }
+
+    private fun initUI() {
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        adapter = TodoAdapter(this, this)
+        binding.recyclerView.adapter = adapter
+
+        val getContent =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val todo = result.data?.getSerializableExtra("todo") as? Todo
+                    if (todo != null) {
+                        viewModel.insertTodo(todo)
+                    }
+                }
+            }
+
+        binding.fabAddTodo.setOnClickListener {
+            val intent = Intent(this, AddTodoActivity::class.java)
+            getContent.launch(intent)
+        }
+
+    }
+
+    private val updateOrDeleteTodo =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val todo = result.data?.getSerializableExtra("todo") as Todo
+                val isDelete = result.data?.getBooleanExtra("delete_todo", false) as Boolean
+                if (todo != null && !isDelete) {
+                    viewModel.updateTodo(todo)
+                }else if(todo != null && isDelete){
+                    viewModel.deleteTodo(todo)
+                }
+            }
+        }
+
+
+
+    override
+    fun onItemClicked(todo: Todo) {
+        val intent = Intent(this@MainActivity, AddTodoActivity::class.java)
+        intent.putExtra("current_todo", todo)
+        updateOrDeleteTodo.launch(intent)
+    }
+
+    @Suppress("DEPRECATION")
+    override fun openGallery() {
+        // For latest versions API LEVEL 19+
+        var intent = Intent(Intent.ACTION_OPEN_DOCUMENT)
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        intent.addCategory(Intent.CATEGORY_OPENABLE)
+        intent.type = "image/*"
+        startActivityForResult(intent, REQUEST_CODE);
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE){
+
+            // if multiple images are selected
+            if (data?.getClipData() != null) {
+                var count = data.clipData?.itemCount
+
+                for (i in 0..count!! - 1) {
+                    var imageUri: Uri = data.clipData?.getItemAt(i)!!.uri
+                    //     iv_image.setImageURI(imageUri) Here you can assign your Image URI to the ImageViews
+                }
+
+            } else if (data?.getData() != null) {
+                // if single image is selected
+
+                var imageUri: Uri = data.data!!
+                //   iv_image.setImageURI(imageUri) Here you can assign the picked image uri to your imageview
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/in/ktechnos/todoassignment/TodoDatabase.kt
@@ -0,0 +1,30 @@
+package `in`.ktechnos.todoassignment
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Todo::class), version = 1)
+abstract class TodoDatabase : RoomDatabase() {
+    abstract fun getTodoDao(): TodoDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: TodoDatabase? = null
+
+        fun getDatabase(context: Context): TodoDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    TodoDatabase::class.java,
+                    "app_database"
+                ).build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
+}
